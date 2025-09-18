c1cc90f551fe6ce1d447b88b294ec61c741f0a16
Myslitel666 <tretyakov_02@bk.ru>
2025-09-18 13:42:26 +0500

    i

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
   import { Pen, Delete, Save } from "svelte-elegant/icons-elegant";
   let isEntryModalOpen = false;
   let isDeleteModalOpen = false;
+  let isInitialized = false;
   let entries = [];
   let boxSize = "220px";
   let modal = {
@@ -59,167 +60,215 @@
     loadEntries();
     isEntryModalOpen = false;
   }
-  onMount(loadEntries);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat("ru-RU", {
+      weekday: "short", // 'Fri' вместо 'Friday'
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }).format(date);
+  }
+
+  onMount(() => {
+    loadEntries();
+    isInitialized = true;
+  });
 </script>
 
-<div class="container">
-  <Button
-    variant="Outlined"
-    width={boxSize}
-    height={boxSize}
-    fontSize="20px"
-    onClick={() => {
-      isEntryModalOpen = true;
-      modal = { title: "", content: "", date: "", isCreate: true };
-    }}
-  >
-    <div class="create-button flex">
-      <Pen size="45px" />
-      <p style:color={$themeStore.palette.primary}>Create New Diary Entry</p>
-    </div>
-  </Button>
-  {#each entries as e}
+{#if isInitialized}
+  <div class="container">
     <Button
       variant="Outlined"
-      borderColor={$themeStore.border.elegant.color}
-      bgColorHover={$themeStore.surface.ghost.background}
       width={boxSize}
       height={boxSize}
+      fontSize="20px"
       onClick={() => {
         isEntryModalOpen = true;
         modal = {
-          id: e.id,
-          title: e.title,
-          content: e.content,
-          date: e.date,
-          isCreate: false,
+          title: "",
+          content: "",
+          date: "",
+          isCreate: true,
         };
       }}
     >
-      <div class="flex entry" style:width={boxSize} style:height={boxSize}>
-        <p style:font-weight="600" style:color={$themeStore.palette.primary}>
-          {e.title}
-        </p>
-        <p>{e.content}</p>
-        <p
-          class="created-time"
-          style:color={$themeMode === "light" ? "#ccc" : "#555"}
-        >
-          {e.date.slice(0, 10)}
-        </p>
+      <div class="create-button flex">
+        <Pen size="45px" />
+        <p style:color={$themeStore.palette.primary}>Create New Diary Entry</p>
       </div>
     </Button>
-  {/each}
-  <Modal bind:isOpen={isEntryModalOpen} width="400px">
-    <div style:width="100%" style:display="flex" style:justify-content="center">
-      <p
-        style:font-size="24px"
-        style:font-weight="600"
-        style:margin-bottom="14px"
-      >
-        {#if modal.isCreate}Create{:else}Modify{/if} Entry
-      </p>
-    </div>
-    <TextField
-      bind:value={modal.date}
-      width="100%"
-      type="date"
-      label="Date"
-      required
-    />
-    <div style:margin-top="9px">
-      <TextField bind:value={modal.title} width="100%" label="Title" required />
-    </div>
-    <div style:margin-top="9px">
-      <TextArea bind:value={modal.content} width="100%" label="Content" />
-    </div>
-    {#if modal.isCreate}
-      <Button onClick={addEntry} marginTop="9px" width="100%">
-        <Save size="23px" fill={$themeStore.palette.text.contrast} />
-        <span style:margin-left="8px">SAVE ENTRY</span>
-      </Button>
-    {:else}
-      <Button
-        onClick={() => {
-          updateEntry();
-        }}
-        marginTop="9px"
-        width="100%"
-      >
-        <Pen size="23px" fill={$themeStore.palette.text.contrast} />
-        <span style:margin-left="8px">MODIFY ENTRY</span>
-      </Button>
-    {/if}
-    {#if !modal.isCreate}
+    {#each entries as e}
       <Button
         variant="Outlined"
-        borderColor="#f50d0d"
-        color="#f50d0d"
-        marginTop="9px"
-        bgColorHover="rgba(255,0,0,0.12)"
-        width="100%"
-        onClick={() => {
-          isEntryModalOpen = false;
-          isDeleteModalOpen = true;
-        }}
-      >
-        <Delete fill="#f50d0d" size="25px" />
-        <span style:margin-left="5px">DELETE ENTRY</span>
-      </Button>
-    {/if}
-  </Modal>
-
-  <Modal bind:isOpen={isDeleteModalOpen} width="auto">
-    <div
-      style:width="100%"
-      style:display="flex"
-      style:flex-direction="column"
-      style:justify-content="center"
-      style:text-align="center"
-    >
-      <p
-        style:font-size="24px"
-        style:font-weight="600"
-        style:margin-bottom="5px"
-      >
-        Delete Entry
-      </p>
-      <p style:margin-bottom="5px">
-        Are you sure you want to delete this entry?
-      </p>
-    </div>
-    <div
-      style:width="100%"
-      style:display="flex"
-      style:justify-content="center"
-      style:gap="7px"
-    >
-      <Button
-        variant="Text"
-        borderColor="#f50d0d"
-        color="#f50d0d"
-        bgColorHover="rgba(255,0,0,0.12)"
-        width="100px"
-        onClick={() => {
-          deleteEntry();
-          isDeleteModalOpen = false;
-        }}
-      >
-        <span style:margin-left="px">YES</span>
-      </Button>
-      <Button
-        variant="Text"
-        width="100px"
+        borderColor={$themeStore.border.elegant.color}
+        bgColorHover={$themeStore.surface.ghost.background}
+        width={boxSize}
+        height={boxSize}
         onClick={() => {
           isEntryModalOpen = true;
-          isDeleteModalOpen = false;
+          modal = {
+            id: e.id,
+            title: e.title,
+            content: e.content,
+            date: e.date,
+            isCreate: false,
+          };
         }}
       >
-        <span style:width="30px">NO</span>
+        <div
+          class="flex entry"
+          style:width={`calc(${boxSize} - 25px)`}
+          style:height={boxSize}
+        >
+          <p style:font-weight="600" style:color={$themeStore.palette.primary}>
+            {e.title}
+          </p>
+          <!-- style:text-align="justify"-->
+          <p style:line-height="1.5">{e.content}</p>
+          <p
+            class="created-time"
+            style:color={$themeMode === "light" ? "#ccc" : "#555"}
+          >
+            {formatDate(e.date)}
+          </p>
+        </div>
       </Button>
-    </div>
-  </Modal>
-</div>
+    {/each}
+    <Modal bind:isOpen={isEntryModalOpen} width="400px">
+      <div
+        style:width="100%"
+        style:display="flex"
+        style:justify-content="center"
+      >
+        <p
+          style:font-size="24px"
+          style:font-weight="600"
+          style:margin-bottom="14px"
+        >
+          {#if modal.isCreate}Create{:else}Modify{/if} Entry
+        </p>
+      </div>
+      <TextField
+        bind:value={modal.date}
+        width="100%"
+        type="date"
+        label="Date"
+        required
+      />
+      <div style:margin-top="9px">
+        <TextField
+          bind:value={modal.title}
+          width="100%"
+          label="Title"
+          required
+        />
+      </div>
+      <div style:margin-top="9px">
+        <TextArea bind:value={modal.content} width="100%" label="Content" />
+      </div>
+      {#if modal.isCreate}
+        <Button onClick={addEntry} marginTop="9px" width="100%">
+          <Save size="23px" fill={$themeStore.palette.text.contrast} />
+          <span style:margin-left="8px">SAVE ENTRY</span>
+        </Button>
+      {:else}
+        <div
+          style:width="100%"
+          style:display="flex"
+          style:justify-content="center"
+        >
+          <Button
+            variant="Text"
+            marginTop="6px"
+            width="100px"
+            height="25px"
+            padding="0"
+          >
+            Check Details
+          </Button>
+        </div>
+        <Button
+          onClick={() => {
+            updateEntry();
+          }}
+          marginTop="6px"
+          width="100%"
+        >
+          <Pen size="23px" fill={$themeStore.palette.text.contrast} />
+          <span style:margin-left="8px">MODIFY ENTRY</span>
+        </Button>
+        <Button
+          variant="Outlined"
+          borderColor="#f50d0d"
+          color="#f50d0d"
+          marginTop="9px"
+          bgColorHover="rgba(255,0,0,0.12)"
+          width="100%"
+          onClick={() => {
+            isEntryModalOpen = false;
+            isDeleteModalOpen = true;
+          }}
+        >
+          <Delete fill="#f50d0d" size="25px" />
+          <span style:margin-left="5px">DELETE ENTRY</span>
+        </Button>
+      {/if}
+    </Modal>
+
+    <Modal bind:isOpen={isDeleteModalOpen} width="auto">
+      <div
+        style:width="100%"
+        style:display="flex"
+        style:flex-direction="column"
+        style:justify-content="center"
+        style:text-align="center"
+      >
+        <p
+          style:font-size="24px"
+          style:font-weight="600"
+          style:margin-bottom="5px"
+        >
+          Delete Entry
+        </p>
+        <p style:margin-bottom="5px">
+          Are you sure you want to delete this entry?
+        </p>
+      </div>
+      <div
+        style:width="100%"
+        style:display="flex"
+        style:justify-content="center"
+        style:gap="7px"
+      >
+        <Button
+          variant="Text"
+          borderColor="#f50d0d"
+          color="#f50d0d"
+          bgColorHover="rgba(255,0,0,0.12)"
+          width="100px"
+          onClick={() => {
+            deleteEntry();
+            isDeleteModalOpen = false;
+          }}
+        >
+          <span style:margin-left="px">YES</span>
+        </Button>
+        <Button
+          variant="Text"
+          width="100px"
+          onClick={() => {
+            isEntryModalOpen = true;
+            isDeleteModalOpen = false;
+          }}
+        >
+          <span style:width="30px">NO</span>
+        </Button>
+      </div>
+    </Modal>
+  </div>
+{/if}
 
 <style>
   .container {
